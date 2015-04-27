6c825be47c357f783d032191531d1286b834d34c
fungku <fungku@gmail.com>
2015-04-27 16:24:02 -0700

    fix tests and preceeding slash issue, hopefully

--- spec/PostmarkSpec.php
+++ spec/PostmarkSpec.php
@@ -25,7 +25,6 @@ class PostmarkSpec extends ObjectBehavior
 
     function let(Filesystem $filesystem, Parser $parser)
     {
-
         $this->beConstructedWith($filesystem, $parser);
     }
 
@@ -37,102 +36,97 @@ class PostmarkSpec extends ObjectBehavior
     function it_gets_content_for_file(Filesystem $filesystem, Parser $parser)
     {
         $filePath = $this->postPath .'.md';
-        
-        $content = [
-            'title' => 'File',
-            'breadcrumbs' => [
-                ['href' => '/My',           'name' => 'My'],
-                ['href' => '/My/Post',      'name' => 'Post'],
-                ['href' => '/My/Post/File', 'name' => 'File']
-            ],
-            'index' => [],
-            'post'  => $this->html,
-        ];
 
-        $filesystem->isDirectory($this->postPath)
-            ->willReturn(false);
-        $filesystem->exists($filePath)
-            ->willReturn(true);
-        $filesystem->get($filePath)
-            ->willReturn($this->markdown);
+        $this->setupFilesystemMethods($filesystem, $filePath);
 
         $parser->parse($this->markdown)
-            ->willReturn($content['post']);
+            ->willReturn($this->html);
 
         $this->getContent($this->basePath, $this->post)
-            ->shouldReturn($content);
+             ->shouldReturn([
+                'title' => 'File',
+                'breadcrumbs' => [
+                    ['href' => '/My',           'name' => 'My'],
+                    ['href' => '/My/Post',      'name' => 'Post'],
+                    ['href' => '/My/Post/File', 'name' => 'File']
+                ],
+                'index' => [],
+                'post'  => $this->html,
+            ]);
     }
 
     function it_gets_content_for_directory_with_index_only(Filesystem $filesystem, Parser $parser)
     {
         $filePath = $this->postPath .'/index.md';
 
-        $content = [
-            'title' => 'File',
-            'breadcrumbs' => [
-                ['href' => '/My',           'name' => 'My'],
-                ['href' => '/My/Post',      'name' => 'Post'],
-                ['href' => '/My/Post/File', 'name' => 'File'],
-            ],
-            'index' => ['subcategories' => [], 'files' => []],
-            'post'  => $this->html,
-        ];
-
-        $filesystem->isDirectory($this->postPath)
-            ->willReturn(true);
-        $filesystem->exists($filePath)
-            ->willReturn(true);
-        $filesystem->get($filePath)
-            ->willReturn($this->markdown);
-        $filesystem->directories($this->postPath)
-            ->willReturn([]);
-        $filesystem->files($this->postPath)
-            ->willReturn([]);
+        $this->setupFilesystemMethods($filesystem, $filePath, true);
 
         $parser->parse($this->markdown)
-            ->willReturn($content['post']);
+            ->willReturn($this->html);
 
         $this->getContent($this->basePath, $this->post)
-            ->shouldReturn($content);
+             ->shouldReturn([
+                'title' => 'File',
+                'breadcrumbs' => [
+                    ['href' => '/My',           'name' => 'My'],
+                    ['href' => '/My/Post',      'name' => 'Post'],
+                    ['href' => '/My/Post/File', 'name' => 'File'],
+                ],
+                'index' => ['subcategories' => [], 'files' => []],
+                'post'  => $this->html,
+            ]);
     }
 
     function it_gets_content_for_directory_with_subcategories_and_files(Filesystem $filesystem, Parser $parser)
     {
         $filePath = $this->postPath .'/index.md';
 
-        $content = [
-            'title' => 'File',
-            'breadcrumbs' => [
-                ['href' => '/My',           'name' => 'My'],
-                ['href' => '/My/Post',      'name' => 'Post'],
-                ['href' => '/My/Post/File', 'name' => 'File'],
-            ],
-            'index' => [
-                'subcategories' => [
-                    ['href' => '/Some-Subcategory', 'name' => 'Some Subcategory'],
-                ],
-                'files' => [
-                    ['href' => '/Another-Post', 'name' => 'Another Post'],
-                ],
-            ],
-            'post' => $this->html,
-        ];
+        $this->setupFilesystemMethods($filesystem, $filePath, true, [$this->postPath.'/Some-Subcategory'], [$this->postPath.'/Another-Post.md', $this->postPath . '/Test-Post.md']);
 
+        $parser->parse($this->markdown)
+            ->willReturn($this->html);
+
+        $this->getContent($this->basePath, $this->post)
+             ->shouldReturn([
+                'title' => 'File',
+                'breadcrumbs' => [
+                    ['href' => '/My', 'name' => 'My'],
+                    ['href' => '/My/Post', 'name' => 'Post'],
+                    ['href' => '/My/Post/File', 'name' => 'File'],
+                ],
+                'index' => [
+                    'subcategories' => [
+                        ['href' => '/My/Post/File/Some-Subcategory', 'name' => 'Some Subcategory'],
+                    ],
+                    'files' => [
+                        ['href' => '/My/Post/File/Another-Post', 'name' => 'Another Post'],
+                        ['href' => '/My/Post/File/Test-Post', 'name' => 'Test Post'],
+                    ],
+                ],
+                'post' => $this->html,
+            ]);
+    }
+
+    /**
+     * Set up the filesystem doubles.
+     *
+     * @param Filesystem $filesystem
+     * @param string $filePath
+     * @param bool $isDir
+     * @param array $returnDirs
+     * @param array $returnFiles
+     */
+    private function setupFilesystemMethods($filesystem, $filePath, $isDir = false, $returnDirs= [], $returnFiles = [])
+    {
         $filesystem->isDirectory($this->postPath)
-            ->willReturn(true);
+            ->willReturn($isDir);
         $filesystem->exists($filePath)
             ->willReturn(true);
         $filesystem->get($filePath)
             ->willReturn($this->markdown);
         $filesystem->directories($this->postPath)
-            ->willReturn(['/Some-Subcategory']);
+            ->willReturn($returnDirs);
         $filesystem->files($this->postPath)
-            ->willReturn(['Another-Post.md']);
-
-        $parser->parse($this->markdown)
-            ->willReturn($content['post']);
-
-        $this->getContent($this->basePath, $this->post)
-            ->shouldReturn($content);
+            ->willReturn($returnFiles);
     }
 }
